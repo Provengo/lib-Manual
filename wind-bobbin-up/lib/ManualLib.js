3b240c026054275ae92ea97849f75681b1cdae27
Michael Bar-Sinai <mich.barsinai@gmail.com>
2024-10-14 19:10:58 +0300

    More resilient obj-to-html

--- wind-bobbin-up/lib/ManualLib.js
+++ wind-bobbin-up/lib/ManualLib.js
@@ -17,6 +17,49 @@ const Manual = (function(){
         return (!!e.data) && e.data.lib == __LIB_SIG__;
     });
 
+    const IGNORED_KEYS = ["lib", "type", "session"];
+    /**
+     * Utility function for representing JavaScript values in HTML.
+     * @param {*} aValue The value to be represented.
+     * @returns A HTML view of `aValue`.
+     */
+    function valueToHtml(aValue) {
+        try {
+            let objType = (typeof aValue);
+            switch (objType ){
+                case "undefined":
+                    return "undefined";
+
+                case "object":
+                    if ( Array.isArray(aValue) ) {
+                        let items = [];
+                        for ( let idx in aValue ) {
+                            items.push(`<li>${valueToHtml(aValue[idx])}</li>`);
+                        }
+                        return "<ol>" + items.join("") + "</ol>";
+                        
+                    } else {
+                        let lis = [];
+                        for ( let k of Object.keys(aValue) ) {
+                            if ( IGNORED_KEYS && IGNORED_KEYS.indexOf(k) > -1 ) {
+                                continue;
+                            }
+                            let aSubValue = aValue[k];
+                            let htmlValue = valueToHtml(aSubValue, IGNORED_KEYS);
+                            
+                            lis.push(`<dt>${k}</dt><dd>${htmlValue}</dd>`);
+                        }
+                        return "<dl>" + (lis.join("")) + "</dl>";
+                    }
+
+                default:
+                    return String(aValue);
+            }
+        } catch (e) {
+            return JSON.stringify(aValue) + "<br><div style='font-size: 9pt; color:#ff8888; font-family:monospace'>(html generation error: " + e + ")</div>";
+        }
+    }
+
     function validationEvent(session, condition, details) {
         let e = evt("check: " + condition, "validation", session);
         e.data.condition = condition;
@@ -80,7 +123,7 @@ const Manual = (function(){
             case "action":
                 body = e.data.action
                 if ( e.data.validation ){
-                    details +=`<div><label style="color:#070">🔎 Validate:</label> <span style="font-style: italic">${e.data.validation}</span></div>`;
+                    details +=`<div><label style="color:#070">🔎 Validate:</label> <span style="font-style: italic">${valueToHtml(e.data.validation)}</span></div>`;
                 }
                 break;
             case "validation":
